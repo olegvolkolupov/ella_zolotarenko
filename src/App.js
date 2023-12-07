7c6e80a400653574d8749c3fa3c5ffdfedd32ca2
olegvolkolupov <olegvolkolupov@gmail.com>
2023-12-07 22:12:34 +0100

    ella_portfolio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <header></header>
+      <h1>Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
